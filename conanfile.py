77955398bb27098ddf950b866e2db1f5c48275d4
Björn Stresing <bjoern.stresing@gmx.de>
2018-11-19 22:58:06 +0100

    Feature: Installer

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ class LS50RemoteConan(ConanFile):
     author = jsonInfo["vendor"]
     homepage = jsonInfo["repository"]
     requires = "Qt/[>=5.10]@tereius/stable"
-    settings = "os", "compiler", "build_type", "arch"
+    settings = {"os": ["Windows"], "compiler": None, "build_type": None, "arch": None}
     options = {"portable": [True, False]}
     default_options = "portable=True", "Qt:shared=True", "Qt:qtbase=True", "Qt:GUI=True", "Qt:qttranslations=True", "Qt:qttools=True", "Qt:qtsvg=True", "Qt:widgets=True", "Qt:qtdeclarative=True", "Qt:qtgraphicaleffects=True", "Qt:qtquickcontrols2=True"
     generators = "cmake"
@@ -23,10 +23,16 @@ class LS50RemoteConan(ConanFile):
         cmake = CMake(self)
         if self.options.portable:
             cmake.definitions["APP_PORTABLE_MODE"] = True
+            cmake.definitions["CPACK_GENERATOR"] = "ZIP"
         else:
             cmake.definitions["APP_PORTABLE_MODE"] = False
+            cmake.definitions["CPACK_GENERATOR"] = "NSIS"
         cmake.configure()
         return cmake
+
+    def build_requirements(self):
+        if self.settings.os == "Windows" and not self.options.portable:
+            self.build_requires("NSIS/3.03@tereius/stable")
 
     def build(self):
         tools.replace_in_file(os.path.join(self.build_folder, "CMakeLists.txt"), "### CONAN_BEACON ###", 'include(%s)\n%s' % (os.path.join(self.install_folder, "conanbuildinfo.cmake").replace("\\", "/"), "conan_basic_setup()"), strict=False)
@@ -36,3 +42,7 @@ class LS50RemoteConan(ConanFile):
     def package(self):
         cmake = self.configure_cmake()
         cmake.build(target="PACKAGE")
+        if self.options.portable:
+            self.copy("LS50Remote.zip")
+        else:
+            self.copy("LS50Remote.exe")
