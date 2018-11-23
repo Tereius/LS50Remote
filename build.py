4575e71ea00d5587fccedd01fa16db3a779e6c81
Björn Stresing <bjoern.stresing@gmx.de>
2018-11-23 21:54:02 +0100

    Appveyor ci

---
+++ build.py
@@ -0,0 +1,49 @@
+from conan.packager import ConanMultiPackager
+from subprocess import check_output
+import os
+
+if __name__ == "__main__":
+
+    os.system("conan profile new ./ci-profile")
+    if 'CONAN_BASE_PROFILE_OS' in os.environ:
+        os.system("conan profile update settings.os=\"%s\" ./ci-profile" % os.environ['CONAN_BASE_PROFILE_OS'])
+
+    if 'CONAN_BASE_PROFILE_OS_API' in os.environ:
+        os.system("conan profile update settings.os.api_level=\"%s\" ./ci-profile" % os.environ['CONAN_BASE_PROFILE_OS_API'])
+
+    if 'CONAN_BASE_PROFILE_OSBUILD' in os.environ:
+        os.system("conan profile update settings.os_build=\"%s\" ./ci-profile" % os.environ['CONAN_BASE_PROFILE_OSBUILD'])
+
+    if 'CONAN_BASE_PROFILE_ARCH' in os.environ:
+        os.system("conan profile update settings.arch=\"%s\" ./ci-profile" % os.environ['CONAN_BASE_PROFILE_ARCH'])
+
+    if 'CONAN_BASE_PROFILE_ARCHBUILD' in os.environ:
+        os.system("conan profile update settings.arch_build=\"%s\" ./ci-profile" % os.environ['CONAN_BASE_PROFILE_ARCHBUILD'])
+
+    if 'CONAN_BASE_PROFILE_COMPILER' in os.environ:
+        os.system("conan profile update settings.compiler=\"%s\" ./ci-profile" % os.environ['CONAN_BASE_PROFILE_COMPILER'])
+
+    if 'CONAN_BASE_PROFILE_COMPILER_VERSION' in os.environ:
+        os.system("conan profile update settings.compiler.version=\"%s\" ./ci-profile" % os.environ['CONAN_BASE_PROFILE_COMPILER_VERSION'])
+
+    if 'CONAN_BASE_PROFILE_COMPILER_LIBCXX' in os.environ:
+        os.system("conan profile update settings.compiler.libcxx=\"%s\" ./ci-profile" % os.environ['CONAN_BASE_PROFILE_COMPILER_LIBCXX'])
+
+    if 'CONAN_BASE_PROFILE_BUILDTYPE' in os.environ:
+        os.system("conan profile update settings.build_type=\"%s\" ./ci-profile" % os.environ['CONAN_BASE_PROFILE_BUILDTYPE'])
+
+    user_name = "user"
+    user_channel = "testing"
+    if 'CONAN_USERNAME' in os.environ:
+        user_name = os.environ['CONAN_USERNAME']
+
+    if 'CONAN_CHANNEL' in os.environ:
+        user_channel = os.environ['CONAN_CHANNEL']
+    
+    builder = ConanMultiPackager()
+    builder.add()
+    builder.run(os.path.abspath("./ci-profile"))
+
+    version = check_output("conan inspect . -a version").output.decode("ascii")
+    name = check_output("conan inspect . -a name").output.decode("ascii")
+    check_output("conan install %s/%s@%s/%s -pr %s" % (name[6:], version[9:], user_name, user_channel, "ci-profile"))
